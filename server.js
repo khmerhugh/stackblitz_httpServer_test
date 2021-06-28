877789fb724d5d19ecc99375240f36d49d04e5be
khmerhugh <hugh@boilermaker.net>
2021-06-28 09:08:31 -0400

    1 file modified

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ var fs = require('fs');
 
 http
   .createServer(function(req, res) {
-    const text = "Boilermaker.Net!";
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html')
-    res.end(text)
+    const text = 'Boilermaker.Net!';
+    text += '<br>Hugh Johnson</br>';
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end(text);
   })
   .listen(8080); //the server object listens on port 8080
 
